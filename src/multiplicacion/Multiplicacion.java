0c662a2a2be81e022f043b31e915ad7db897454a
Amarico <Amarico@equipo-zalman>
2018-09-02 08:41:23 +0200

    muestra en pantalla un número multiplicado por otro
    El número ha de ser introducido por teclado

---
+++ src/multiplicacion/Multiplicacion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiplicacion;
+
+/**Realiza un programa que pida dos números 
+ * y que luego muestre el resultado de su multiplicación
+ *
+ * @author Amarico
+ */
+public class Multiplicacion {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
